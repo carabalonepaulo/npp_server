7b888bfdf94886d6b9198990511e2406da433528
Paulo Carbalone <psoreto@outlook.com>
2023-01-25 01:17:11 -0300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,10 @@ mod module;
 use async_std::{channel::unbounded, task};
 use events::{ListenerEvent, LuaEvent};
 
-fn main() -> generic_result::GenericResult<()> {
+fn main() {
     let (listener_sender, listener_receiver) = unbounded::<ListenerEvent>();
     let (lua_sender, lua_receiver) = unbounded::<LuaEvent>();
 
     task::spawn(lua::run(lua_sender, listener_receiver));
     task::block_on(listener::run(listener_sender, lua_receiver));
-
-    Ok(())
 }
